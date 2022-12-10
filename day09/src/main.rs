e9c790152c75fd3abc58598aa9751357036da542
Lag <lagaillardie.nicolas@live.fr>
2022-12-10 13:36:02 +0100

    improved day09 code with factorisation and removed unnecessary as f32

--- day09/src/main.rs
+++ day09/src/main.rs
@@ -5,6 +5,16 @@ use std::fs::File;
 use std::io::{stdin, BufRead, BufReader};
 use std::path::Path;
 
+/// Update position of tail compared to head
+fn update_cell_position(head_cell: &(i32, i32), tail_cell: &mut (i32, i32)) {
+    if (head_cell.0 - tail_cell.0).abs().pow(2) + (head_cell.1 - tail_cell.1).abs().pow(2) > 2 {
+        tail_cell.0 =
+            (head_cell.0 - tail_cell.0) / max(1, (head_cell.0 - tail_cell.0).abs()) + tail_cell.0;
+        tail_cell.1 =
+            (head_cell.1 - tail_cell.1) / max(1, (head_cell.1 - tail_cell.1).abs()) + tail_cell.1;
+    }
+}
+
 /// Function for part 01
 fn aux_one(file: &Path) -> usize {
     // Open file
@@ -20,9 +30,8 @@ fn aux_one(file: &Path) -> usize {
     let mut explored_cells = vec![node_01_cell];
 
     // Read file line by line, for part 01
-    // Retrieve all integers in vec of vec
     for (_index_line, line) in reader.lines().enumerate() {
-        // Split all integers and parse them into u32
+        // Split line into direction and steps
         let line = line.unwrap();
         let instruction = line.split(' ').collect::<Vec<&str>>();
 
@@ -51,21 +60,7 @@ fn aux_one(file: &Path) -> usize {
                 err => panic!("Error, wrong direction, expected L, R, U or D, found {err}"),
             }
 
-            let distance = (i32::abs(node_01_cell.0 - head_cell.0).pow(2)
-                + i32::abs(node_01_cell.1 - head_cell.1).pow(2)) as f32;
-
-            if distance > 2.0 {
-                node_01_cell.0 = (head_cell.0 - node_01_cell.0)
-                    / max(1, i32::abs(head_cell.0 - node_01_cell.0))
-                    + node_01_cell.0;
-                node_01_cell.1 = (head_cell.1 - node_01_cell.1)
-                    / max(1, i32::abs(head_cell.1 - node_01_cell.1))
-                    + node_01_cell.1;
-            }
-
-            println!("distance: {distance}");
-            println!("head_cell: {:?}", head_cell);
-            println!("node_01_cell: {:?}", node_01_cell);
+            update_cell_position(&head_cell, &mut node_01_cell);
 
             explored_cells.push(node_01_cell);
         }
@@ -88,7 +83,7 @@ fn aux_two(file: &Path) -> usize {
 
     let reader = BufReader::new(file);
 
-    // Starting cell of head and tail
+    // Starting cells
     let mut head_cell = (0, 0);
     let mut node_01_cell = (0, 0);
     let mut node_02_cell = (0, 0);
@@ -104,9 +99,8 @@ fn aux_two(file: &Path) -> usize {
     let mut node_09_explored_cells = vec![node_09_cell];
 
     // Read file line by line, for part 01
-    // Retrieve all integers in vec of vec
     for (_index_line, line) in reader.lines().enumerate() {
-        // Split all integers and parse them into u32
+        // Split line into direction and steps
         let line = line.unwrap();
         let instruction = line.split(' ').collect::<Vec<&str>>();
 
@@ -136,130 +130,31 @@ fn aux_two(file: &Path) -> usize {
             }
 
             // Head to node 01
-            let distance_01_head = (i32::abs(node_01_cell.0 - head_cell.0).pow(2)
-                + i32::abs(node_01_cell.1 - head_cell.1).pow(2))
-                as f32;
-
-            if distance_01_head > 2.0 {
-                node_01_cell.0 = (head_cell.0 - node_01_cell.0)
-                    / max(1, i32::abs(head_cell.0 - node_01_cell.0))
-                    + node_01_cell.0;
-                node_01_cell.1 = (head_cell.1 - node_01_cell.1)
-                    / max(1, i32::abs(head_cell.1 - node_01_cell.1))
-                    + node_01_cell.1;
-            }
+            update_cell_position(&head_cell, &mut node_01_cell);
 
             // Node 02 to node 01
-            let distance_02_01 = (i32::abs(node_01_cell.0 - node_02_cell.0).pow(2)
-                + i32::abs(node_01_cell.1 - node_02_cell.1).pow(2))
-                as f32;
-
-            if distance_02_01 > 2.0 {
-                node_02_cell.0 = (node_01_cell.0 - node_02_cell.0)
-                    / max(1, i32::abs(node_01_cell.0 - node_02_cell.0))
-                    + node_02_cell.0;
-                node_02_cell.1 = (node_01_cell.1 - node_02_cell.1)
-                    / max(1, i32::abs(node_01_cell.1 - node_02_cell.1))
-                    + node_02_cell.1;
-            }
+            update_cell_position(&node_01_cell, &mut node_02_cell);
 
             // Node 03 to node 02
-            let distance_03_02 = (i32::abs(node_02_cell.0 - node_03_cell.0).pow(2)
-                + i32::abs(node_02_cell.1 - node_03_cell.1).pow(2))
-                as f32;
-
-            if distance_03_02 > 2.0 {
-                node_03_cell.0 = (node_02_cell.0 - node_03_cell.0)
-                    / max(1, i32::abs(node_02_cell.0 - node_03_cell.0))
-                    + node_03_cell.0;
-                node_03_cell.1 = (node_02_cell.1 - node_03_cell.1)
-                    / max(1, i32::abs(node_02_cell.1 - node_03_cell.1))
-                    + node_03_cell.1;
-            }
+            update_cell_position(&node_02_cell, &mut node_03_cell);
 
             // Node 04 to node 03
-            let distance_04_03 = (i32::abs(node_03_cell.0 - node_04_cell.0).pow(2)
-                + i32::abs(node_03_cell.1 - node_04_cell.1).pow(2))
-                as f32;
-
-            if distance_04_03 > 2.0 {
-                node_04_cell.0 = (node_03_cell.0 - node_04_cell.0)
-                    / max(1, i32::abs(node_03_cell.0 - node_04_cell.0))
-                    + node_04_cell.0;
-                node_04_cell.1 = (node_03_cell.1 - node_04_cell.1)
-                    / max(1, i32::abs(node_03_cell.1 - node_04_cell.1))
-                    + node_04_cell.1;
-            }
+            update_cell_position(&node_03_cell, &mut node_04_cell);
 
             // Node 05 to node 04
-            let distance_05_04 = (i32::abs(node_04_cell.0 - node_05_cell.0).pow(2)
-                + i32::abs(node_04_cell.1 - node_05_cell.1).pow(2))
-                as f32;
-
-            if distance_05_04 > 2.0 {
-                node_05_cell.0 = (node_04_cell.0 - node_05_cell.0)
-                    / max(1, i32::abs(node_04_cell.0 - node_05_cell.0))
-                    + node_05_cell.0;
-                node_05_cell.1 = (node_04_cell.1 - node_05_cell.1)
-                    / max(1, i32::abs(node_04_cell.1 - node_05_cell.1))
-                    + node_05_cell.1;
-            }
+            update_cell_position(&node_04_cell, &mut node_05_cell);
 
             // Node 06 to node 05
-            let distance_06_05 = (i32::abs(node_05_cell.0 - node_06_cell.0).pow(2)
-                + i32::abs(node_05_cell.1 - node_06_cell.1).pow(2))
-                as f32;
-
-            if distance_06_05 > 2.0 {
-                node_06_cell.0 = (node_05_cell.0 - node_06_cell.0)
-                    / max(1, i32::abs(node_05_cell.0 - node_06_cell.0))
-                    + node_06_cell.0;
-                node_06_cell.1 = (node_05_cell.1 - node_06_cell.1)
-                    / max(1, i32::abs(node_05_cell.1 - node_06_cell.1))
-                    + node_06_cell.1;
-            }
+            update_cell_position(&node_05_cell, &mut node_06_cell);
 
             // Node 07 to node 06
-            let distance_07_06 = (i32::abs(node_06_cell.0 - node_07_cell.0).pow(2)
-                + i32::abs(node_06_cell.1 - node_07_cell.1).pow(2))
-                as f32;
-
-            if distance_07_06 > 2.0 {
-                node_07_cell.0 = (node_06_cell.0 - node_07_cell.0)
-                    / max(1, i32::abs(node_06_cell.0 - node_07_cell.0))
-                    + node_07_cell.0;
-                node_07_cell.1 = (node_06_cell.1 - node_07_cell.1)
-                    / max(1, i32::abs(node_06_cell.1 - node_07_cell.1))
-                    + node_07_cell.1;
-            }
+            update_cell_position(&node_06_cell, &mut node_07_cell);
 
             // Node 08 to node 07
-            let distance_08_07 = (i32::abs(node_07_cell.0 - node_08_cell.0).pow(2)
-                + i32::abs(node_07_cell.1 - node_08_cell.1).pow(2))
-                as f32;
-
-            if distance_08_07 > 2.0 {
-                node_08_cell.0 = (node_07_cell.0 - node_08_cell.0)
-                    / max(1, i32::abs(node_07_cell.0 - node_08_cell.0))
-                    + node_08_cell.0;
-                node_08_cell.1 = (node_07_cell.1 - node_08_cell.1)
-                    / max(1, i32::abs(node_07_cell.1 - node_08_cell.1))
-                    + node_08_cell.1;
-            }
+            update_cell_position(&node_07_cell, &mut node_08_cell);
 
             // Node 09 to node 08
-            let distance_09_08 = (i32::abs(node_08_cell.0 - node_09_cell.0).pow(2)
-                + i32::abs(node_08_cell.1 - node_09_cell.1).pow(2))
-                as f32;
-
-            if distance_09_08 > 2.0 {
-                node_09_cell.0 = (node_08_cell.0 - node_09_cell.0)
-                    / max(1, i32::abs(node_08_cell.0 - node_09_cell.0))
-                    + node_09_cell.0;
-                node_09_cell.1 = (node_08_cell.1 - node_09_cell.1)
-                    / max(1, i32::abs(node_08_cell.1 - node_09_cell.1))
-                    + node_09_cell.1;
-            }
+            update_cell_position(&node_08_cell, &mut node_09_cell);
 
             node_09_explored_cells.push(node_09_cell);
         }
