3a1050899d4044336ec682a6e1a9d022bff7c8f9
Lag <lagaillardie.nicolas@live.fr>
2022-12-15 18:10:09 +0100

    'solved' part 02 day 14, still waiting for restuls

--- day14/src/main.rs
+++ day14/src/main.rs
@@ -83,11 +83,7 @@ fn aux_one(file: &Path) -> usize {
         }
     }
 
-    println!("coordinates_lines_sand: {:?}", coordinates_lines_sand);
-
-    println!("threshold: {threshold}");
-
-    let mut coordinates_rock = Vec::new();
+    let mut coordinates_rocks = Vec::new();
 
     loop {
         let mut current_rock = (500, 0);
@@ -109,12 +105,10 @@ fn aux_one(file: &Path) -> usize {
         }
 
         if !rest {
-            println!("coordinates_lines_sand: {:?}", coordinates_lines_sand);
-            println!("coordinates_rock: {:?}", coordinates_rock);
-            return coordinates_rock.len();
+            return coordinates_rocks.len();
         } else {
             coordinates_lines_sand.push(current_rock.clone());
-            coordinates_rock.push(current_rock.clone());
+            coordinates_rocks.push(current_rock.clone());
         }
     }
 }
@@ -124,9 +118,113 @@ fn aux_two(file: &Path) -> usize {
     // Open file
     let file = File::open(file).unwrap();
 
-    let _reader = BufReader::new(file);
+    let reader = BufReader::new(file);
 
-    0
+    let mut coordinates_lines_sand = Vec::new();
+
+    let mut threshold = 0;
+
+    // Read file line by line, for part 01
+    // Get composition of each monkey
+    for (_index_line, line) in reader.lines().enumerate() {
+        // Split line into direction and steps
+        let line = line.unwrap();
+        let line = line.trim();
+
+        if !line.is_empty() {
+            let line = line
+                .split(" -> ")
+                .map(|elt| elt.to_string())
+                .collect::<Vec<String>>();
+
+            let mut coordinates = Vec::new();
+
+            let mut temp_previous_coordinates = (-1, -1);
+
+            for elt in line.iter() {
+                // There are only two integers
+                let temp_elt = elt
+                    .split(",")
+                    .map(|elt| elt.parse::<i32>().unwrap())
+                    .collect::<Vec<i32>>();
+
+                if temp_previous_coordinates == (-1, -1) {
+                    temp_previous_coordinates = (temp_elt[0], temp_elt[1]);
+                } else {
+                    // If vertical or horizontal
+                    if temp_previous_coordinates.0 == temp_elt[0] {
+                        if temp_previous_coordinates.1 < temp_elt[1] {
+                            for index in temp_previous_coordinates.1..=temp_elt[1] {
+                                if threshold < index {
+                                    threshold = index;
+                                }
+                                coordinates.push((temp_previous_coordinates.0, index));
+                            }
+                        } else {
+                            for index in (temp_elt[1]..=temp_previous_coordinates.1).rev() {
+                                if threshold < index {
+                                    threshold = index;
+                                }
+                                coordinates.push((temp_previous_coordinates.0, index));
+                            }
+                        }
+                    } else if temp_previous_coordinates.1 == temp_elt[1] {
+                        if temp_previous_coordinates.0 < temp_elt[0] {
+                            for index in temp_previous_coordinates.0..=temp_elt[0] {
+                                coordinates.push((index, temp_previous_coordinates.1));
+                            }
+                        } else {
+                            for index in (temp_elt[0]..=temp_previous_coordinates.0).rev() {
+                                coordinates.push((index, temp_previous_coordinates.1));
+                            }
+                        }
+                    } else {
+                        panic!(
+                            "Wrong coordinates between {:?} and {:?}",
+                            temp_previous_coordinates, temp_elt
+                        );
+                    }
+
+                    temp_previous_coordinates = (temp_elt[0], temp_elt[1]);
+                }
+            }
+
+            coordinates_lines_sand.append(&mut coordinates);
+        }
+    }
+
+    let mut coordinates_rocks = Vec::new();
+
+    loop {
+        let mut current_rock = (500, 0);
+
+        let mut rest = false;
+
+        while current_rock.1 < threshold + 1 && !rest {
+            if !coordinates_lines_sand.contains(&(current_rock.0, current_rock.1 + 1)) {
+                current_rock.1 = current_rock.1 + 1;
+            } else if !coordinates_lines_sand.contains(&(current_rock.0 - 1, current_rock.1 + 1)) {
+                current_rock.0 = current_rock.0 - 1;
+                current_rock.1 = current_rock.1 + 1;
+            } else if !coordinates_lines_sand.contains(&(current_rock.0 + 1, current_rock.1 + 1)) {
+                current_rock.0 = current_rock.0 + 1;
+                current_rock.1 = current_rock.1 + 1;
+            } else {
+                rest = true;
+            }
+        }
+
+        println!("current rocks: {:?}", coordinates_rocks.len());
+
+        if current_rock == (500, 0) {
+            coordinates_lines_sand.push(current_rock.clone());
+            coordinates_rocks.push(current_rock.clone());
+            return coordinates_rocks.len();
+        } else {
+            coordinates_lines_sand.push(current_rock.clone());
+            coordinates_rocks.push(current_rock.clone());
+        }
+    }
 }
 
 /// Main function
@@ -169,6 +267,6 @@ mod tests {
     #[test]
     fn internal() {
         assert_eq!(aux_one(Path::new("input/test.txt")), 24);
-        // assert_eq!(aux_two(Path::new("input/test.txt")), 2713310158);
+        assert_eq!(aux_two(Path::new("input/test.txt")), 93);
     }
 }
