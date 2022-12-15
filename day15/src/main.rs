ca08ffba384afbb7bccf208df0f4cb49dcc64d84
Lag <lagaillardie.nicolas@live.fr>
2022-12-15 22:03:51 +0100

    solved part 02 day 15

--- day15/src/main.rs
+++ day15/src/main.rs
@@ -5,7 +5,7 @@ use std::io::{stdin, BufRead, BufReader};
 use std::path::Path;
 
 /// Function for part 01
-fn aux_one(file: &Path, row: i32) -> i32 {
+fn aux_one(file: &Path, row: i128) -> i128 {
     // Open file
     let file = File::open(file).unwrap();
 
@@ -33,24 +33,24 @@ fn aux_one(file: &Path, row: i32) -> i32 {
         let sensor = sensor.split("Sensor at ").collect::<Vec<&str>>()[1];
         let coordinates_sensor = sensor.split(", ").collect::<Vec<&str>>();
         let x_sensor = coordinates_sensor[0].split("=").collect::<Vec<&str>>()[1];
-        let x_sensor = x_sensor.parse::<i32>().unwrap();
+        let x_sensor = x_sensor.parse::<i128>().unwrap();
         let y_sensor = coordinates_sensor[1].split("=").collect::<Vec<&str>>()[1];
-        let y_sensor = y_sensor.parse::<i32>().unwrap();
+        let y_sensor = y_sensor.parse::<i128>().unwrap();
 
         // Get coordinates of beacon
         let beacon = line[1].clone();
         let coordinates_beacon = beacon.split(", ").collect::<Vec<&str>>();
         let x_beacon = coordinates_beacon[0].split("=").collect::<Vec<&str>>()[1];
-        let x_beacon = x_beacon.parse::<i32>().unwrap();
+        let x_beacon = x_beacon.parse::<i128>().unwrap();
         let y_beacon = coordinates_beacon[1].split("=").collect::<Vec<&str>>()[1];
-        let y_beacon = y_beacon.parse::<i32>().unwrap();
+        let y_beacon = y_beacon.parse::<i128>().unwrap();
 
         // Add coordinates to sensors and beacons
         sensors.push((x_sensor, y_sensor));
         beacons.push((x_beacon, y_beacon));
 
         // Include distance between sensor and beacon
-        let distance = i32::abs(x_sensor - x_beacon) + i32::abs(y_sensor - y_beacon);
+        let distance = i128::abs(x_sensor - x_beacon) + i128::abs(y_sensor - y_beacon);
         distances.push(distance);
 
         // Get lower bound of coverage
@@ -86,7 +86,7 @@ fn aux_one(file: &Path, row: i32) -> i32 {
     for index in min_column..=max_column {
         if !beacons.contains(&(index, row)) {
             'sensor_for: for (index_sensor, sensor) in sensors.iter().enumerate() {
-                if (i32::abs(sensor.0 - index) + i32::abs(sensor.1 - row))
+                if (i128::abs(sensor.0 - index) + i128::abs(sensor.1 - row))
                     <= distances[index_sensor]
                 {
                     closed_positions.push((index, row));
@@ -98,18 +98,173 @@ fn aux_one(file: &Path, row: i32) -> i32 {
 
     println!("closed_positions: {:?}", closed_positions);
 
-    // Include both external columns and zero,
-    // Exclude available positions
-    // And beacons already present
-    closed_positions.len() as i32
+    closed_positions.len() as i128
 }
 
 /// Function for part 02
-fn aux_two(file: &Path) -> i32 {
+fn aux_two(file: &Path, is_test: bool) -> i128 {
     // Open file
     let file = File::open(file).unwrap();
 
-    let _reader = BufReader::new(file);
+    let reader = BufReader::new(file);
+
+    let mut sensors = Vec::new();
+
+    let mut beacons = Vec::new();
+
+    let mut distances = Vec::new();
+
+    let mut min_column = None;
+    let mut max_column = None;
+
+    let mut min_row = None;
+    let mut max_row = None;
+
+    let mut cells_to_test = Vec::new();
+
+    let max_bound = if is_test { 20 } else { 4000000 };
+
+    // Read file line by line, for part 01
+    // Get composition of each monkey
+    for line in reader.lines() {
+        // Split line into direction and steps
+        let line = line.unwrap();
+
+        let line = line.split(": closest beacon is at ").collect::<Vec<&str>>();
+
+        // Get coordinates of sensor
+        let sensor = line[0].clone();
+        let sensor = sensor.split("Sensor at ").collect::<Vec<&str>>()[1];
+        let coordinates_sensor = sensor.split(", ").collect::<Vec<&str>>();
+        let x_sensor = coordinates_sensor[0].split("=").collect::<Vec<&str>>()[1];
+        let x_sensor = x_sensor.parse::<i128>().unwrap();
+        let y_sensor = coordinates_sensor[1].split("=").collect::<Vec<&str>>()[1];
+        let y_sensor = y_sensor.parse::<i128>().unwrap();
+
+        // Get coordinates of beacon
+        let beacon = line[1].clone();
+        let coordinates_beacon = beacon.split(", ").collect::<Vec<&str>>();
+        let x_beacon = coordinates_beacon[0].split("=").collect::<Vec<&str>>()[1];
+        let x_beacon = x_beacon.parse::<i128>().unwrap();
+        let y_beacon = coordinates_beacon[1].split("=").collect::<Vec<&str>>()[1];
+        let y_beacon = y_beacon.parse::<i128>().unwrap();
+
+        // Add coordinates to sensors and beacons
+        sensors.push((x_sensor, y_sensor));
+        beacons.push((x_beacon, y_beacon));
+
+        // Include distance between sensor and beacon
+        let distance = i128::abs(x_sensor - x_beacon) + i128::abs(y_sensor - y_beacon);
+        distances.push(distance);
+
+        // Get lower bound of X coverage
+        if let Some(mini) = min_column {
+            if mini > min(x_sensor - distance, x_beacon) {
+                min_column = Some(min(x_sensor - distance, x_beacon));
+            }
+        } else {
+            min_column = Some(min(x_sensor - distance, x_beacon));
+        }
+
+        // Get upper bound of X coverage
+        if let Some(maxi) = max_column {
+            if maxi < max(x_sensor + distance, x_beacon) {
+                max_column = Some(max(x_sensor + distance, x_beacon));
+            }
+        } else {
+            max_column = Some(max(x_sensor + distance, x_beacon));
+        }
+
+        // Get lower bound of Y coverage
+        if let Some(mini) = min_row {
+            if mini > min(y_sensor - distance, y_beacon) {
+                min_row = Some(min(y_sensor - distance, y_beacon));
+            }
+        } else {
+            min_row = Some(min(y_sensor - distance, y_beacon));
+        }
+
+        // Get upper bound of Y coverage
+        if let Some(maxi) = max_row {
+            if maxi < max(y_sensor + distance, y_beacon) {
+                max_row = Some(max(y_sensor + distance, y_beacon));
+            }
+        } else {
+            max_row = Some(max(y_sensor + distance, y_beacon));
+        }
+
+        for x in 0..((distance + 1) * 4) {
+            if x < (distance + 1) {
+                let cell_x = x_sensor + (x % (distance + 1));
+                if cell_x >= 0 && cell_x <= max_bound {
+                    let cell_y = y_sensor + (x % (distance + 1)) - (distance + 1);
+                    if cell_y >= 0 && cell_y <= max_bound {
+                        cells_to_test.push((cell_x, cell_y));
+                    }
+                }
+            } else if x < 2 * (distance + 1) {
+                let cell_x = x_sensor + (distance + 1) - (x % (distance + 1));
+                if cell_x >= 0 && cell_x <= max_bound {
+                    let cell_y = y_sensor + (x % (distance + 1));
+                    if cell_y >= 0 && cell_y <= max_bound {
+                        cells_to_test.push((cell_x, cell_y));
+                    }
+                }
+            } else if x < 3 * (distance + 1) {
+                let cell_x = x_sensor - (x % (distance + 1));
+                if cell_x >= 0 && cell_x <= max_bound {
+                    let cell_y = y_sensor + (distance + 1) - (x % (distance + 1));
+                    if cell_y >= 0 && cell_y <= max_bound {
+                        cells_to_test.push((cell_x, cell_y));
+                    }
+                }
+            } else if x < 4 * (distance + 1) {
+                let cell_x = x_sensor - (distance + 1) + (x % (distance + 1));
+                if cell_x >= 0 && cell_x <= max_bound {
+                    let cell_y = y_sensor - (x % (distance + 1));
+                    if cell_y >= 0 && cell_y <= max_bound {
+                        cells_to_test.push((cell_x, cell_y));
+                    }
+                }
+            }
+        }
+    }
+
+    println!("sensors: {:?}", sensors);
+    println!("beacons: {:?}", beacons);
+
+    let min_column = min_column.unwrap();
+    let max_column = max_column.unwrap();
+
+    println!("min_column: {:?}", min_column);
+    println!("max_column: {:?}", max_column);
+
+    let min_row = min_row.unwrap();
+    let max_row = max_row.unwrap();
+
+    println!("min_row: {:?}", min_row);
+    println!("max_row: {:?}", max_row);
+
+    for (index, cell) in cells_to_test.iter().enumerate() {
+        if !beacons.contains(&cell) {
+            let mut is_available = true;
+
+            'sensor_for: for (index_sensor, sensor) in sensors.iter().enumerate() {
+                if (i128::abs(sensor.0 - cell.0) + i128::abs(sensor.1 - cell.1))
+                    <= distances[index_sensor]
+                {
+                    is_available = false;
+                    break 'sensor_for;
+                }
+            }
+
+            println!("Progress: {:?}", index as f64 / cells_to_test.len() as f64);
+
+            if is_available {
+                return cell.0 * 4000000 + cell.1;
+            }
+        }
+    }
 
     0
 }
@@ -138,15 +293,17 @@ fn main() {
         .expect("Failed to read input");
 
     // Ask row
-    println!("Which row is tested");
-
     let mut row = String::new();
 
-    stdin().read_line(&mut row).expect("Failed to read input");
+    if choice.trim() == "1" {
+        println!("Which row is tested");
+
+        stdin().read_line(&mut row).expect("Failed to read input");
+    }
 
     let result = match choice.trim() {
-        "1" => aux_one(file, row.trim().parse::<i32>().unwrap()),
-        "2" => aux_two(file),
+        "1" => aux_one(file, row.trim().parse::<i128>().unwrap()),
+        "2" => aux_two(file, false),
         _ => panic!("Error, expecting 1 or 2"),
     };
 
@@ -161,6 +318,6 @@ mod tests {
     #[test]
     fn internal() {
         assert_eq!(aux_one(Path::new("input/test.txt"), 10), 26);
-        // assert_eq!(aux_two(Path::new("input/test.txt")), 93);
+        assert_eq!(aux_two(Path::new("input/test.txt"), true), 56000011);
     }
 }
