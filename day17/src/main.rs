cc339dae7d67f802ec4d92ee019e7c7ef3f00b87
Lag <lagaillardie.nicolas@live.fr>
2022-12-18 00:32:21 +0100

    init day 17

---
+++ day17/src/main.rs
@@ -0,0 +1,89 @@
+use core::panic;
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::{stdin, BufRead, BufReader};
+use std::path::Path;
+
+/// Function for part 01
+fn aux_one(file: &Path) -> i32 {
+    // Open file
+    let file = File::open(file).unwrap();
+
+    let reader = BufReader::new(file);
+
+    let mut valves = HashMap::<String, Valve>::new();
+    let mut valves_with_pressure = HashMap::<String, Valve>::new();
+
+    // Read file line by line, for part 01
+    // Get composition of each monkey
+    for line in reader.lines() {
+        // Split line into direction and steps
+        let line = line.unwrap();
+    }
+
+    0
+}
+
+/// Function for part 02
+fn aux_two(file: &Path) -> i32 {
+    // Open file
+    let file = File::open(file).unwrap();
+
+    let reader = BufReader::new(file);
+
+    let mut valves = HashMap::<String, Valve>::new();
+    let mut valves_with_pressure = HashMap::<String, Valve>::new();
+
+    // Read file line by line, for part 01
+    // Get composition of each monkey
+    for line in reader.lines() {
+        // Split line into direction and steps
+        let line = line.unwrap();
+    }
+
+    0
+}
+
+/// Main function
+fn main() {
+    println!("Enter path to file");
+
+    // Ask input of path
+    let mut path = String::new();
+
+    stdin().read_line(&mut path).expect("Failed to read input");
+
+    // Remove end of input containing \n
+    let path = path.trim();
+
+    let file = Path::new(path);
+
+    // Ask input of part
+    println!("Which part do you want? (1/2)");
+
+    let mut choice = String::new();
+
+    stdin()
+        .read_line(&mut choice)
+        .expect("Failed to read input");
+
+    let result = match choice.trim() {
+        "1" => aux_one(file),
+        "2" => aux_two(file),
+        _ => panic!("Error, expecting 1 or 2"),
+    };
+
+    // Display total score
+    println!("Result: {:?}", result);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn internal() {
+        // assert_eq!(aux_one(Path::new("input/test.txt")), 1651);
+        // assert_eq!(aux_two(Path::new("input/test.txt")), 1707);
+    }
+}
